2e3467bbfc092f0480e1d2a75a0c36278c6d36c7
aissya <ailyssya163.com>
2020-09-11 00:33:30 +0800

    第一次新建项目

---
+++ src/main/java/com/ly/SpringRun.java
@@ -0,0 +1,14 @@
+package com.ly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringRun {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRun.class, args);
+		System.out.println("第一次新建项目");
+	}
+}
