d2aacdb3dce5db9ebc853b7574c1c4a7e03478c0
aissya <ailyssya163.com>
2020-09-11 00:34:48 +0800

    第一次更新项目

--- src/main/java/com/ly/SpringRun.java
+++ src/main/java/com/ly/SpringRun.java
@@ -10,5 +10,7 @@ public class SpringRun {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRun.class, args);
 		System.out.println("第一次新建项目");
+		System.out.println("第一次更新项目");
+		
 	}
 }
